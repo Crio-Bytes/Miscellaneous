d51541b5b50998f579fc1a5e96d55ae4463cdcde
Deepak Kumar <prajapati1999.in@gmail.com>
2020-10-29 23:42:00 +0530

    All files uploaded

---
+++ Video Player/src/MainActivity.java
@@ -0,0 +1,19 @@
+package com.riseinsteps.videodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+
+//        write your code here
+    }
+}
