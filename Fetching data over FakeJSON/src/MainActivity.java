94dc5274f025a04006f5b91cf137f07b9b69fe99
Deepak Kumar <prajapati1999.in@gmail.com>
2020-10-30 00:04:23 +0530

    Files Uploaded

---
+++ Fetching data over FakeJSON/src/MainActivity.java
@@ -0,0 +1,34 @@
+package com.codewithharry.volleydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(this);
+
+// write your code here
+
+    }
+}
+
