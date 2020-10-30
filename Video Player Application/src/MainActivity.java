6a6db95895540e781e163f6591f5313f94d92585
Deepak Kumar <prajapati1999.in@gmail.com>
2020-10-30 17:55:57 +0530

    Files Updated

--- Video Player Application/src/MainActivity.java
+++ Video Player Application/src/MainActivity.java
@@ -1,9 +1,7 @@
 package com.riseinsteps.videodemo;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
+
 
 public class MainActivity extends AppCompatActivity {
 
